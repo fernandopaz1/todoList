c4d971bf28a5987142ec2d46393eaf0ef99b1ed4
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-16 01:20:30 -0300

    add: template base y lista de ToDo

--- todoList/todo/models.py
+++ todoList/todo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
 class Todo(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,6 +11,15 @@ class Todo(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    state = models.CharField( default='backlog',max_length=20,
+        choices=(
+            ("backlog", "Backlog"),
+            ("bloqueada", "Bloqueada"),
+            ("enProceso", "En proceso"),
+            ("avanzada", "avanzada"),
+            ("atrazada", "Atrasada"),
+            ("terminada", "Terminada")
+        ))
     
     def publish(self):
         self.published_date = timezone.now()
